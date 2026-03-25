4a7c02021933e44fcf14459d3087d8854a854af2
fritzipaar <frp0157@thi.de>
2026-03-25 17:46:09 +0100

    halbe Aufgabe 4

---
+++ Lehrveranstaltung.java
@@ -0,0 +1,9 @@
+public class Lehrveranstaltung {
+    private String titel;
+    private String dozierende;
+    private String studierende;
+    private int vorlesungsstunden;
+    private int praktikumsstunden;
+    
+    public Lehrveranstaltung(String titel, String dozierende, String studierende, int vorlesungsstunden)
+}
