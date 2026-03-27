fcd73cdf05d93c8f57e829ce9b3998d432c868a9
fritzipaar <frp0157@thi.de>
2026-03-27 12:48:47 +0100

    Fertig

--- Praktikumsstunden.java
+++ Praktikumsstunden.java
@@ -1,3 +1,12 @@
 public class Praktikumsstunden {
-    
+     
+    private String thema;
+    private int datum;
+    private int uhrzeit;
+    private int raum;
+    private String aufgaben;
+
+    public void detailsVorlesung(String thema, int datum, int uhrzeit, int raum, String aufgaben) {
+        System.out.println("Details zur Vorlesung sind: " + thema + ", " + datum + ", "+ uhrzeit + ", " + raum + ", " + aufgaben);
+    }
 }
--- Vorlesungsstunde.java
+++ Vorlesungsstunde.java
@@ -1,3 +1,11 @@
 public class Vorlesungsstunde {
     
+    private String thema;
+    private int datum;
+    private int uhrzeit;
+    private int raum;
+
+    public void detailsVorlesung(String thema, int datum, int uhrzeit, int raum) {
+        System.out.println("Details zur Vorlesung sind: " + thema + ", " + datum + ", "+ uhrzeit + ", " + raum);
+    }
 }
