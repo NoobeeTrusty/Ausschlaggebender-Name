84abb07fe433ef46e35c5ca7670cd47455217c20
fritzipaar <frp0157@thi.de>
2026-03-27 13:00:35 +0100

    Wirklich fertig

--- Vorlesungsstunde.java
+++ Vorlesungsstunde.java
@@ -5,7 +5,14 @@ public class Vorlesungsstunde {
     private int uhrzeit;
     private int raum;
 
-    public void detailsVorlesung(String thema, int datum, int uhrzeit, int raum) {
+    public Vorlesungsstunde(String thema, int datum, int uhrzeit, int raum){
+        this.thema = thema;
+        this.datum = datum;
+        this.uhrzeit = uhrzeit;
+        this.raum = raum;
+    }
+
+    public void detailsVorlesung() {
         System.out.println("Details zur Vorlesung sind: " + thema + ", " + datum + ", "+ uhrzeit + ", " + raum);
     }
 }
